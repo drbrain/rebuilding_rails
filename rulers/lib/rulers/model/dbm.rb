abe4b7294490daf5398cd386809c2b94c03b12aa
Eric Hodel <drbrain@segment7.net>
2020-07-29 13:53:20 -0700

    Add some documentation

--- rulers/lib/rulers/model/dbm.rb
+++ rulers/lib/rulers/model/dbm.rb
@@ -7,6 +7,9 @@ require "json"
 class Rulers::Model::DBM
   @dbm = nil
 
+  ##
+  # Retrieve all records from the DB.
+
   def self.all
     dbm.each_key.map { |id|
       find id
@@ -24,6 +27,9 @@ class Rulers::Model::DBM
       end
   end
 
+  ##
+  # Find a record by ID
+
   def self.find id
     id = String id
 
@@ -31,6 +37,12 @@ class Rulers::Model::DBM
 
     new id
   end
+
+  ##
+  # Load or create a record from the database.
+  #
+  # If no +id+ is given the "next" id is chosen and used.  See #new_id for
+  # caveats when creating a new record.
 
   def initialize id = new_id
     @id = String id
@@ -46,9 +58,17 @@ class Rulers::Model::DBM
       end
   end
 
+  ##
+  # Retrieve the value for +field+.
+
   def [] field
     @attributes[field]
   end
+
+  ##
+  # Set +field+ to +value+.
+  #
+  # The value will not be persisted until #save is called.
 
   def []= field, value
     @attributes[field] = value
@@ -60,13 +80,26 @@ class Rulers::Model::DBM
   # DBM doesn't expose a transactional interface so it's possible that record
   # creation will collide.  To potentially minimize this we immediately store
   # an empty record in the id we found.
+  #
+  # This should use the ruby-bdb gem which has transactions, but I didn't want
+  # to add the dependency.
+  #
+  #--
+  #
+  # This method is called before #initialize so we need to use
+  # <code>self.class.dbm</code> instead of <code>@dbm</code>.
 
   def new_id
     dbm = self.class.dbm
+
     id = dbm.keys.map { |k| k.to_i }.max + 1
     dbm[id.to_s] ||= ""
+
     id.to_s
   end
+
+  ##
+  # Saves the attributes to the DB.
 
   def save
     json = JSON.dump @attributes
@@ -75,6 +108,9 @@ class Rulers::Model::DBM
 
     true
   end
+
+  ##
+  # Merges +attributes+ into the model's attributes
 
   def update_attributes attributes
     @attributes.merge! attributes
